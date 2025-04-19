a6d101699727c9a28b849258ba56a211bb19b3ff
Mukund Prasad <183606131+MukundXplore@users.noreply.github.com>
2025-04-19 17:34:31 +0530

    Add files via upload
    Signed-off-by: Mukund Prasad <183606131+MukundXplore@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPopup() {
+    document.getElementById('popup').style.display = 'flex';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+setTimeout(showPopup, 3000);
